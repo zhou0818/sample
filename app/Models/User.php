1398270def5352bbf1dc249b66756392b2a78778
zhou <95430589@qq.com>
2017-05-02 09:58:00 +0000

    user

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,8 @@ use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
+use Auth;
+
 class User extends Model implements
     AuthenticatableContract,
                                     AuthorizableContract,
@@ -59,7 +61,7 @@ class User extends Model implements
     }
 
     public function feed()
-<<<<<<< HEAD
+
     {
         $user_ids = Auth::user()->followings->pluck('id')->toArray();
         array_push($user_ids, Auth::user()->id);
@@ -68,40 +70,7 @@ class User extends Model implements
                              ->orderBy('created_at', 'desc');
     }
 
-    public function followers()
-    {
-        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
-    }
 
-    public function followings()
-    {
-        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
-    }
-
-    public function follow($user_ids)
-    {
-        if (!is_array($user_ids)) {
-            $user_ids = compact('user_ids');
-        }
-        $this->followings()->sync($user_ids, false);
-    }
-
-    public function unfollow($user_ids)
-    {
-        if (!is_array($user_ids)) {
-            $user_ids = compact('user_ids');
-        }
-        $this->followings()->detach($user_ids);
-    }
-
-=======
-   {
-       $user_ids = Auth::user()->followings->pluck('id')->toArray();
-       array_push($user_ids, Auth::user()->id);
-       return Status::whereIn('user_id', $user_ids)
-                             ->with('user')
-                             ->orderBy('created_at', 'desc');
-   }
 
     public function followers()
     {
@@ -129,7 +98,7 @@ class User extends Model implements
         $this->followings()->detach($user_ids);
     }
 
->>>>>>> following-users
+
     public function isFollowing($user_id)
     {
         return $this->followings->contains($user_id);
